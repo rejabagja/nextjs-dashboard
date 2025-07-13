c408fdbcf7e11c1bb236c066be93e821ee0df56c
rejabagja <rezasubagja5@gmail.com>
2025-07-13 13:32:41 +0700

    feat: add customer data table with search functionality
    - Add table component to display customer data from backend
    - Implement dynamic table updates based on search queries

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,23 @@
 import type { Metadata } from 'next';
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function CustomersPage() {
-  return <p>Customers Page</p>;
+export default async function CustomersPage(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+      <Table customers={customers} />
+    </div>
+  );
 }
